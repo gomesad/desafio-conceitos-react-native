a76993a1d1e89e70c21686cab9029074c1e53713
Anderson Figueredo Gomes <gomes.ad@gmail.com>
2020-05-09 22:51:04 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,73 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api';
+
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`);
+    let repositoriesNew = repositories;
+
+    if(response.status === 200) {
+      const likes = response.data.likes;
+      repositoriesNew = repositories.map(repository => {
+        if(repository.id === id) {
+          repository.likes = likes
+        }
+        return repository;
+      });
+    }
+
+    setRepositories(repositoriesNew); 
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
+      <FlatList
+          data={repositories}
+          keyExtractor={(repository) => repository.id}
+          renderItem={({item: repository}) => (
+            <View style={styles.repositoryContainer}>
+            <Text style={styles.repository}>{repository.title}
             </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+  
+            <View style={styles.techsContainer}>
+              {repository.techs.map(tech => {
+                return <Text key={tech} style={styles.tech}>{tech}</Text>
+              })}
+            </View>
+  
+            <View style={styles.likesContainer}>
+              <Text
+                style={styles.likeText}
+                // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                testID={`repository-likes-${repository.id}`}
+              >
+                {repository.likes} {repository.likes === 1 ? "curtida" : "curtidas"}
+              </Text>
+            </View>
+  
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleLikeRepository(repository.id)}
+              // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+              testID={`like-button-${repository.id}`}
             >
-              3 curtidas
-            </Text>
+              <Text style={styles.buttonText}>Curtir</Text>
+            </TouchableOpacity>
           </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+          )}
+        />
       </SafeAreaView>
     </>
   );
@@ -72,19 +99,21 @@ const styles = StyleSheet.create({
   },
   techsContainer: {
     flexDirection: "row",
+    flexWrap: "wrap",
     marginTop: 10,
   },
   tech: {
     fontSize: 12,
     fontWeight: "bold",
     marginRight: 10,
+    marginBottom: 10,
     backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
   },
   likesContainer: {
-    marginTop: 15,
+    marginTop: 5,
     flexDirection: "row",
     justifyContent: "space-between",
   },
